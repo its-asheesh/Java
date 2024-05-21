9de3a56ac83e69fe86244a4b91632d125d104f6b
Asheesh Rathore <153936386+its-asheesh@users.noreply.github.com>
2024-05-21 23:13:45 +0530

    Add files via upload

---
+++ CreateArrayList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class CreateArrayList{
+	public static void main(String args[]){
+		List<Integer>arr = new ArrayList<Integer>();
+		arr.add(10);
+		arr.add(20);
+		arr.add(30);
+		arr.add(40);
+		arr.add(50);
+		System.out.println("ArrayList : "+arr);
+	
+	}
+
+}
