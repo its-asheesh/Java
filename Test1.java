6f87103ddfaf1c80bf7d37ab2add171f10625cce
Asheesh Rathore <153936386+its-asheesh@users.noreply.github.com>
2024-07-22 01:08:20 +0530

    Delete Test1.java

--- Test1.java
+++
@@ -1,9 +0,0 @@
-//Bitwise AND
-class Test1{
-  public static void main(String[] args){
-    int a=3;
-    int b=6;
-    System.out.println("Bitwise AND of "+a+" and "+b+" is "+(a&b));
-  }
-}
-//Output:2
